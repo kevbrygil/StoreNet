98f21c39ccfd296cb2507c28c6ec9d139f6849d2
Gilbert Tamayo <tamayo_144@hotmail.com>
2023-08-01 10:24:32 -0600

    JWT implement finished

--- StoreNet.API/Program.cs
+++ StoreNet.API/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using StoreNet.Application;
 using StoreNet.Application.Sales;
 using System.Reflection;
@@ -25,7 +26,33 @@ builder.Services.AddCors(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "StoreNet API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 builder.Services.AddApplication(builder.Configuration);
 
 builder.Services.AddAuthentication(options =>
--- StoreNet.Service/AuthService.cs
+++ StoreNet.Service/AuthService.cs
@@ -3,16 +3,27 @@ using StoreNet.Domain.Interfaces;
 using StoreNet.Domain.Interfaces.Services;
 using StoreNet.Domain.Interfaces.Repositories;
 using StoreNet.Domain;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace StoreNet.Service
 {
     public class AuthService: IAuthService
     {
         private readonly IUnitOfWorkUser _unitOfWorkUser;
+        public IConfiguration _Configuration { get; }
 
-        public AuthService(IUnitOfWorkUser unitOfWorkUser)
+        public AuthService(IUnitOfWorkUser unitOfWorkUser, IConfiguration configuration)
         {
             _unitOfWorkUser = unitOfWorkUser;
+            _Configuration = configuration;
         }
         public async Task<User> CreateToken(User userInput)
         {
@@ -21,10 +32,36 @@ namespace StoreNet.Service
                 if (userInput.Password == "" || userInput.Username == "") throw new Exception("Password and username are required");
                 var userRepos = _unitOfWorkUser.UserRepository<User>();
                 var user = await userRepos.FindLoginAsync(userInput) ?? throw new Exception("Username or password is incorrect");
-                user.Token = "test";
+
+                var issuer = _Configuration["Jwt:Issuer"];
+                var audience = _Configuration["Jwt:Audience"];
+                var key = Encoding.ASCII.GetBytes(_Configuration["Jwt:Key"]);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new[]
+                    {
+                        new Claim("Id", Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+                        new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                        new Claim(JwtRegisteredClaimNames.Jti,
+                        Guid.NewGuid().ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddMinutes(5),
+                    Issuer = issuer,
+                    Audience = audience,
+                    SigningCredentials = new SigningCredentials
+                    (new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature)
+                };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var jwtToken = tokenHandler.WriteToken(token);
+                var stringToken = tokenHandler.WriteToken(token);
+
+                user.Token = stringToken;
                 return user;
             }
-            catch (Exception)
+            catch (Exception e)
             {
                 throw;
             }
